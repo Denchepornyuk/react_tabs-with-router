2dfc567d1c94aff7841fd35f0a5e0f0447a4ced8
Denys <denys.cheporniuk@gmail.com>
2019-12-24 12:52:45 +0200

    Updates

---
+++ precache-manifest.ccc609aac4e07b7700241671e6749132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a040f374c26192673ebd34b55eb301e",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "6e067b98905f91aed3f8",
+    "url": "/react_tabs-with-router/static/css/main.88d83140.chunk.css"
+  },
+  {
+    "revision": "3f4d3c2944cd9af55491",
+    "url": "/react_tabs-with-router/static/js/2.f18e7daf.chunk.js"
+  },
+  {
+    "revision": "6e067b98905f91aed3f8",
+    "url": "/react_tabs-with-router/static/js/main.5ccaadf1.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
